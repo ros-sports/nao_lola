7f7a27895c48bb5bcce28aa08b1844bdf567a471
ijnek <kenjibrameld@gmail.com>
2023-08-03 01:13:44 +0000

    switch nao_lola_conversion to use nao_lola_sensor_msgs
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

--- nao_lola_conversion/include/nao_lola_conversion/nao_lola_conversion.hpp
+++ nao_lola_conversion/include/nao_lola_conversion/nao_lola_conversion.hpp
@@ -19,8 +19,8 @@
 
 #include "message_filters/subscriber.h"
 #include "message_filters/time_synchronizer.h"
-#include "nao_sensor_msgs/msg/accelerometer.hpp"
-#include "nao_sensor_msgs/msg/gyroscope.hpp"
+#include "nao_lola_sensor_msgs/msg/accelerometer.hpp"
+#include "nao_lola_sensor_msgs/msg/gyroscope.hpp"
 #include "rclcpp/node.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 
@@ -34,20 +34,20 @@ public:
 
 private:
   // Subscriptions
-  message_filters::Subscriber<nao_sensor_msgs::msg::Accelerometer> accelerometer_sub_;
-  message_filters::Subscriber<nao_sensor_msgs::msg::Gyroscope> gyroscope_sub_;
+  message_filters::Subscriber<nao_lola_sensor_msgs::msg::Accelerometer> accelerometer_sub_;
+  message_filters::Subscriber<nao_lola_sensor_msgs::msg::Gyroscope> gyroscope_sub_;
 
   // Publishers
   rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
 
   // Synchronizer
-  std::shared_ptr<message_filters::TimeSynchronizer<nao_sensor_msgs::msg::Accelerometer,
-    nao_sensor_msgs::msg::Gyroscope>> synchronizer_;
+  std::shared_ptr<message_filters::TimeSynchronizer<nao_lola_sensor_msgs::msg::Accelerometer,
+    nao_lola_sensor_msgs::msg::Gyroscope>> synchronizer_;
 
   // Callbacks
   void synchronizerCallback(
-    const nao_sensor_msgs::msg::Accelerometer::SharedPtr & accelerometer,
-    const nao_sensor_msgs::msg::Gyroscope::SharedPtr & gyroscope);
+    const nao_lola_sensor_msgs::msg::Accelerometer::SharedPtr & accelerometer,
+    const nao_lola_sensor_msgs::msg::Gyroscope::SharedPtr & gyroscope);
 };
 
 }  // namespace nao_lola_conversion
