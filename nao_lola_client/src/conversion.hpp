a45b69eb9b61fb7726e0c061d3b3296bb3d0afd5
Kenji Brameld <kenjibrameld@gmail.com>
2023-11-30 03:43:39 +0000

    convert to nao_lola_sensor_msgs::msg::JointPositions to sensor_msgs::JointState, and publish under "joint_states" topic.
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

---
+++ nao_lola_client/src/conversion.hpp
@@ -0,0 +1,29 @@
+// Copyright 2023 Kenji Brameld
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONVERSION_HPP_
+#define CONVERSION_HPP_
+
+#include "nao_lola_sensor_msgs/msg/joint_positions.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+
+namespace conversion
+{
+
+sensor_msgs::msg::JointState toJointState(
+  const nao_lola_sensor_msgs::msg::JointPositions& joint_positions);
+
+}  // namespace conversion
+
+#endif  // CONVERSION_HPP_
